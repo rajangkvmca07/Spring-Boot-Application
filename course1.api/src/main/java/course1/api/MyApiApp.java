197b28aa55b81fcf464a63fe7ba3dc0b8865b952
Rajan Chaturvedi <rajan.chaturvedi@synapse.com>
2017-07-13 15:44:59 +0530

    Added sys out

--- course1.api/src/main/java/course1/api/MyApiApp.java
+++ course1.api/src/main/java/course1/api/MyApiApp.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyApiApp {
 
 	public static void main(String[] args) {
+		System.out.println("-------Added print Statement-----");
 		SpringApplication.run(MyApiApp.class, args);
 
 	}
